1ea0a0aa373349fec50646b38fe98603bdb216b6
noahsark13 <112363037+noahsark13@users.noreply.github.com>
2025-05-21 16:55:13 -0400

    erm ok

--- Assets/Scripts/Tutorial & UI/MenuManager.cs
+++ Assets/Scripts/Tutorial & UI/MenuManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEditor.SceneView;
+//using static UnityEditor.SceneView;
 
 public class MenuManager : MonoBehaviour
 {
