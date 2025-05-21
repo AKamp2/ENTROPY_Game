fc226b058fe20bb2d44c79c77026f1aff0d9e434
rlawson04 <114245674+rlawson04@users.noreply.github.com>
2025-05-21 11:10:38 -0400

    Dialogue and Sound FX sliders

--- Assets/Scripts/Tutorial & UI/SettingsMenu.cs
+++ Assets/Scripts/Tutorial & UI/SettingsMenu.cs
@@ -4,19 +4,42 @@ using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour 
 {
     public GameObject[] optionsMenus;
+    
+    // Audio setting initialization
+    [SerializeField] private Slider dialogueSlider;
+    [SerializeField] private Slider soundFXSlider;
+
 
     private void Awake()
     {
+       // Makes sure menus are not open when starting 
        CloseMenus();
+       
+    }
+
+    public void SetDialogueVolume(AudioSource audioSource)
+    {
+        audioSource.volume = dialogueSlider.value;
+        Debug.Log(audioSource.volume);
+    }
+
+    public void SetSoundFXVolume(AudioSource audioSource)
+    {
+        audioSource.volume = soundFXSlider.value;
+        Debug.Log(audioSource.volume);
+
     }
 
     // Click handlers
     public void OpenMenu(GameObject menu)
     {
+        // Closes other menus before opening
+        // the menu that matches the section button
         CloseMenus();
         menu.SetActive(true);
     }
