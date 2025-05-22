d6b9b4b3b361c60f15dbc17183365e1c3d8cead1
rlawson04 <114245674+rlawson04@users.noreply.github.com>
2025-05-22 12:24:07 -0400

    Making persistent settings

--- Assets/Scripts/Tutorial & UI/SettingsMenu.cs
+++ Assets/Scripts/Tutorial & UI/SettingsMenu.cs
@@ -5,6 +5,8 @@ using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using UnityEngine.Audio;
+using System;
+using System.IO;
 
 public class SettingsMenu : MonoBehaviour 
 {
@@ -18,28 +20,34 @@ public class SettingsMenu : MonoBehaviour
     // General setting initialization
     [SerializeField] private Toggle subtitleCheckbox;
     [SerializeField] private Slider sensitivitySlider;
+    [SerializeField] private GameObject subtitles;
 
 
     private void Awake()
     {
        // Makes sure menus are not open when starting 
        CloseMenus();
-       
+       dialogueSlider.value = GetPrefs("dialogueSlider",1);
+       soundFXSlider.value = GetPrefs("soundFXSlider",1);
+       sensitivitySlider.value = GetPrefs("sensitivitySlider",4);
+       subtitleCheckbox.isOn = GetPrefs("subtitleCheckbox", 1) == 1;
+        ToggleSubtitles(subtitles);
     }
 
     public void SetDialogueVolume(AudioSource audioSource)
     {
         // Sets the dialogue volume to the slide value
         audioSource.volume = dialogueSlider.value;
-        Debug.Log(audioSource.volume);
+        //Debug.Log(audioSource.volume);
+        SetPrefs("dialogueSlider", dialogueSlider.value);
     }
 
     public void SetSoundFXVolume(AudioSource audioSource)
     {
         // Sets the sound effects volume to the slide value
         audioSource.volume = soundFXSlider.value;
-        Debug.Log(audioSource.volume);
-
+        //Debug.Log(audioSource.volume);
+        SetPrefs("soundFXSlider", soundFXSlider.value);
     }
 
     public void SetSensitivity(ZeroGravity player)
@@ -47,6 +55,7 @@ public class SettingsMenu : MonoBehaviour
         // Sets the sound effects volume to the slide value
         player.SensitivityX = sensitivitySlider.value;
         player.SensitivityY = sensitivitySlider.value;
+        SetPrefs("sensitivitySlider", sensitivitySlider.value);
     }
 
     public void ToggleSubtitles(GameObject dialogueText)
@@ -54,10 +63,12 @@ public class SettingsMenu : MonoBehaviour
         if (subtitleCheckbox.isOn)
         {
             dialogueText.SetActive(true);
+            SetPrefs("subtitleCheckbox", 1);
         }
         else 
         { 
             dialogueText.SetActive(false);
+            SetPrefs("subtitleCheckbox", 0);
         }
     }
 
@@ -75,5 +86,16 @@ public class SettingsMenu : MonoBehaviour
         {
             menu.SetActive(false);
         }
+    }
+
+    void SetPrefs(string keyName, float value)
+    {
+        int intValue = Mathf.RoundToInt(value);
+        PlayerPrefs.SetInt(keyName, intValue);
+    }
+
+    int GetPrefs(string keyName, int defaultValue)
+    {
+        return PlayerPrefs.GetInt(keyName, defaultValue);
     }
 }
