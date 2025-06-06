595d2b40643eb90b1ddd171c6574df952a803cc8
Adam Gayheart <113038629+ajg8716@users.noreply.github.com>
2025-06-06 15:55:59 -0400

    player tweaking

--- Assets/Scripts/Player/ZeroGravity.cs
+++ Assets/Scripts/Player/ZeroGravity.cs
@@ -347,8 +347,6 @@ public class ZeroGravity : MonoBehaviour
                 //handle grabber icon logic
                 if (isGrabbing && grabbedBar != null && canGrab)
                 {
-                   
-
                     if (canGrab)
                     {
                         //handle the grab movement
@@ -374,12 +372,10 @@ public class ZeroGravity : MonoBehaviour
                 if (isGrabbing && grabbedBar != null)
                 {
                     //handle the grab movement
-
                     if (useIK)
                     {
                         AdjustBarGrabbers();
                     }
-                    
                     HandleGrabMovement(grabbedBar);
                 }
             }
