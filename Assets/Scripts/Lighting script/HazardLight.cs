e6e33fcd786e58047b16ad691cda4a595060f2ea
Adam Gayheart <113038629+ajg8716@users.noreply.github.com>
2025-06-25 17:15:57 -0400

    added some mood lighting to the scene

---
+++ Assets/Scripts/Lighting script/HazardLight.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.UIElements;
+
+public class HazardLight : MonoBehaviour
+{
+    [SerializeField]
+    private bool isHazard;
+    [SerializeField]
+    private Light light;
+    [SerializeField] 
+    private Light lightBase;
+    [SerializeField]
+    private float rotateParam;
+
+    public bool IsHazard
+    {
+        get { return isHazard; }
+        set { isHazard = value; }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the hazrd is set
+        if(isHazard)
+        {
+            if(!light.enabled)
+            {
+                light.enabled = true;
+                lightBase.enabled = true;
+            }
+            light.transform.Rotate(transform.up * rotateParam * Time.deltaTime);
+        }
+        else if (!isHazard)
+        {
+            if (light.enabled)
+            {
+                light.enabled = false;
+                lightBase.enabled = false;
+            }
+        }
+    }
+}
