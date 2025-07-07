56d0dc9ff5fac56b04bf48d3f7f768f9de63bd83
Adam Gayheart <113038629+ajg8716@users.noreply.github.com>
2025-07-07 16:15:29 -0400

    triggers for objective dialogue set up

---
+++ Assets/Scripts/Tutorial & UI/DialogueTrigger.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [SerializeField]
+    DialogueManager manager;
+    [SerializeField]
+    float delay = 0f;
+    [SerializeField]
+    private Collider player;
+
+    [SerializeField]
+    int index;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == player)
+        {
+            Debug.Log("player collided");
+            //ensure it only happens
+            this.GetComponent<Collider>().enabled = false;
+            //sart the dialogue
+            manager.StartDialogueSequence(index, delay);
+
+
+        }
+    }
+}
