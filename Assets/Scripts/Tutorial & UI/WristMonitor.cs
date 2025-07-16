44e4f0efe116671937eca6ca211e63fa5d914d9a
rlawson04 <114245674+rlawson04@users.noreply.github.com>
2025-07-16 11:51:59 -0400

    Added objective system

--- Assets/Scripts/Tutorial & UI/WristMonitor.cs
+++ Assets/Scripts/Tutorial & UI/WristMonitor.cs
@@ -1,11 +1,35 @@
-using UnityEngine;
+using NUnit.Framework;
+using System.Collections.Generic;
 using TMPro;
+using UnityEngine;
 
 public class WristMonitor : MonoBehaviour
 {
     bool isActive = false;
     [SerializeField] ZeroGravity player;
-    [SerializeField] TextMeshProUGUI health;
+    [SerializeField] TextMeshProUGUI healthText;
+    [SerializeField] TextMeshProUGUI currentObjectiveText;
+    public List<Objective> objectives = new List<Objective>();
+
+    public class Objective
+    {
+        public Objective(string objectiveName, string objectiveDescription, bool isCompleted)
+        {
+            ObjectiveName = objectiveName;
+            ObjectiveDescription = objectiveDescription;
+            IsCompleted = isCompleted;
+        }
+
+        public string ObjectiveName { get; set; }
+        public string ObjectiveDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    private void Start()
+    {
+        objectives.Add(new Objective("Medbay", "Reach the Medbay", false));
+    }
+
     public void ToggleWristMonitor()
     {
         isActive = !isActive;
@@ -14,6 +38,7 @@ public class WristMonitor : MonoBehaviour
 
     private void Update()
     {
-        health.text = $"Health: {player.PlayerHealth}";
+        healthText.text = $"Health: {player.PlayerHealth}";
+        currentObjectiveText.text = $"<color=orange>Current Objective: </color>\n\t{objectives[0].ObjectiveDescription}";
     }
 }
