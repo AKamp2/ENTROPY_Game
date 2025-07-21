dee1b990a4cb833dbe7a98322f1cc98f7dfeb184
rlawson04 <114245674+rlawson04@users.noreply.github.com>
2025-07-21 15:41:19 -0400

    Added Lerp as well as hold control for Wrist Monitor

--- Assets/Scripts/Tutorial & UI/WristMonitor.cs
+++ Assets/Scripts/Tutorial & UI/WristMonitor.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WristMonitor : MonoBehaviour
 {
@@ -13,6 +14,13 @@ public class WristMonitor : MonoBehaviour
     [SerializeField] GameObject wristMonitor;
     public List<Objective> mainObjectives = new List<Objective>();
     public List<Objective> completedObjectives = new List<Objective>();
+    public RectTransform targetRectTransform;
+    public Vector3 targetPosition;
+    public float lerpDuration;
+
+    Vector3 startPosition;
+    public float duration;
+    Vector3 currentPosition;
 
     /// <summary>
     /// Public class used to display vital information to the player of how they must proceed
@@ -43,29 +51,71 @@ public class WristMonitor : MonoBehaviour
         mainObjectives.Add(new Objective("Dining Room", "<color=orange>Current Objective: </color>\n\tReach the Dining Room", "<size=8><color=orange>Sub Objective: </color>\n\tReconnect ALAN</size>", false));
         mainObjectives.Add(new Objective("Server Room", "<color=orange>Current Objective: </color>\n\tReach the Server Room", "<size=8><color=orange>Sub Objective: </color>\n\tOverride Manual Lockdown</size>", false));
         mainObjectives.Add(new Objective("Facilities Room", "<color=orange>Current Objective: </color>\n\tReach the Facilities Room", "", false));
+        if (targetRectTransform == null) {
+            Debug.LogError("TargetRectTransform not assigned");
+            return;
+        }
+        startPosition = targetRectTransform.anchoredPosition3D;
+        duration = 0f;
     }
+
+
 
     /// <summary>
     /// Public method called by the zero gravity controller to turn the monitor on and off
     /// </summary>
-    public void ToggleWristMonitor()
+    public void ToggleWristMonitor(InputAction.CallbackContext context)
     {
-        if (!wristMonitor.activeSelf)
+        if (context.performed && !wristMonitor.activeSelf)
         {
-            isActive = !isActive;
-            this.gameObject.SetActive(isActive);
+            isActive = true;
+            this.gameObject.SetActive(true);
+            StartLerp();
+        }
+        else if (context.canceled)
+        {
+            isActive = false;
+            StartLerp();
+            
         }
     }
 
+    public void StartLerp()
+    {
+        startPosition = new Vector3(-300,110,0);
+        duration = 0f;
+    }
+
+    
+
     /// <summary>
-    /// Updates health text as well as the current objective
+    /// Updates health text as well as the current objective. Also handles Lerp logic 
     /// </summary>
-    private void Update()
+    private void FixedUpdate()
     {
         healthText.text = $"Health: {player.PlayerHealth}";
         if(mainObjectives.Count > 0)
         {
             currentObjectiveText.text = $"{mainObjectives[0].ObjectiveDescription}\n{mainObjectives[0].SubObjective}";
+        }
+        currentPosition = targetRectTransform.anchoredPosition3D;
+        if(isActive && duration < lerpDuration)
+        {
+            duration += Time.deltaTime;
+            float t = duration / lerpDuration;
+
+            targetRectTransform.anchoredPosition3D = Vector3.Lerp(startPosition, targetPosition, t);
+        }
+        if (!isActive && duration < lerpDuration)
+        {
+            duration += Time.deltaTime;
+            float t = duration / lerpDuration;
+
+            targetRectTransform.anchoredPosition3D = Vector3.Lerp(currentPosition, startPosition, t);
+
+            if (targetRectTransform.anchoredPosition3D == startPosition) {
+                this.gameObject.SetActive(false);
+            }
         }
     }
 
