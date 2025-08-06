fbb6cf0fe73a26ed622ce83cb4dc3633e2026015
Aidan Kamp <ackamp2@gmail.com>
2025-08-06 15:54:59 -0400

    left you some comments in the Terminal manager  for how I would do it

--- Assets/Scripts/Tutorial & UI/TerminalManager.cs
+++ Assets/Scripts/Tutorial & UI/TerminalManager.cs
@@ -5,5 +5,11 @@ public class TerminalManager : MonoBehaviour
 {
 
     [SerializeField] Terminal medbayTerminal;
-   
+
+    //What you can do is move the onInteract into this script so that you don't have to give the player input an OnInteract for every single terminal in the game.
+    //Then have a reference for currentTerminal in here and update what the currently viewed terminal is by passing a reference of the terminal you're looking at to this script in the UIManager.
+    //OnInteract should activate the terminal sequence of the currentTerminal (and it should only do this once, terminals need to be marked as used and you should be checking to see if the terminal is used before displaying the UI)
+    //Things like sound effects will be called from the terminal script so that it can easily be localized to that terminal's audio source.
+    //if you want the interaction to be a hold like on stim dispenser you can look at the stim dispenser script to see how I did it, possibly use the same UI element as I did for the progress bar.
+
 }
