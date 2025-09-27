e9a9c0706b6882fb6d48be35e95a7736d50eaac3
Adam Gayheart <113038629+ajg8716@users.noreply.github.com>
2025-09-26 20:09:04 -0400

    Revert "commented out bulkprefabeditor"
    This reverts commit e6ff9a87b16db0e5bfdf7bdac9bae70f4027a2aa.

--- Assets/Scripts/Dev Tools/BulkPrefabReplacer.cs
+++ Assets/Scripts/Dev Tools/BulkPrefabReplacer.cs
@@ -1,42 +1,42 @@
-//using UnityEngine;
-//using UnityEditor;
+using UnityEngine;
+using UnityEditor;
 
-//public class BulkPrefabReplacer : EditorWindow
-//{
-//    public GameObject prefab;
+public class BulkPrefabReplacer : EditorWindow
+{
+    public GameObject prefab;
 
-//    [MenuItem("Tools/Bulk Prefab Replacer")]
-//    static void Init()
-//    {
-//        GetWindow<BulkPrefabReplacer>("Prefab Replacer");
-//    }
+    [MenuItem("Tools/Bulk Prefab Replacer")]
+    static void Init()
+    {
+        GetWindow<BulkPrefabReplacer>("Prefab Replacer");
+    }
 
-//    void OnGUI()
-//    {
-//        prefab = (GameObject)EditorGUILayout.ObjectField("Prefab", prefab, typeof(GameObject), false);
+    void OnGUI()
+    {
+        prefab = (GameObject)EditorGUILayout.ObjectField("Prefab", prefab, typeof(GameObject), false);
 
-//        if (prefab == null)
-//        {
-//            EditorGUILayout.HelpBox("Assign a prefab to replace with.", MessageType.Warning);
-//            return;
-//        }
+        if (prefab == null)
+        {
+            EditorGUILayout.HelpBox("Assign a prefab to replace with.", MessageType.Warning);
+            return;
+        }
 
-//        if (GUILayout.Button("Replace Selected"))
-//        {
-//            ReplaceSelected();
-//        }
-//    }
+        if (GUILayout.Button("Replace Selected"))
+        {
+            ReplaceSelected();
+        }
+    }
 
-//    void ReplaceSelected()
-//    {
-//        foreach (GameObject go in Selection.gameObjects)
-//        {
-//            GameObject newObj = (GameObject)PrefabUtility.InstantiatePrefab(prefab, go.transform.parent);
-//            newObj.transform.SetPositionAndRotation(go.transform.position, go.transform.rotation);
-//            newObj.transform.localScale = go.transform.localScale;
+    void ReplaceSelected()
+    {
+        foreach (GameObject go in Selection.gameObjects)
+        {
+            GameObject newObj = (GameObject)PrefabUtility.InstantiatePrefab(prefab, go.transform.parent);
+            newObj.transform.SetPositionAndRotation(go.transform.position, go.transform.rotation);
+            newObj.transform.localScale = go.transform.localScale;
 
-//            Undo.RegisterCreatedObjectUndo(newObj, "Replace With Prefab");
-//            Undo.DestroyObjectImmediate(go);
-//        }
-//    }
-//}
+            Undo.RegisterCreatedObjectUndo(newObj, "Replace With Prefab");
+            Undo.DestroyObjectImmediate(go);
+        }
+    }
+}
