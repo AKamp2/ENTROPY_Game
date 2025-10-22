532b5e938e063633f779ea5950d5dd175d8e6284
Tom-Roman <69944327+tfr8811@users.noreply.github.com>
2025-10-22 15:41:49 -0400

    safety check for tutorial manager

--- Assets/Scripts/Tutorial & UI/TutorialManager.cs
+++ Assets/Scripts/Tutorial & UI/TutorialManager.cs
@@ -84,7 +84,7 @@ public class TutorialManager : MonoBehaviour
         pickupScript = ZeroGPlayer.GetComponent<PickupScript>();
         playerGrabRange = playerController.GrabRange;
 
-        if (playerController.TutorialMode == true)
+        if (playerController.TutorialMode == true && !GlobalSaveManager.LoadFromSave)
         {
             dialogueManager.OnDialogueEnd += OnDialogueComplete;
             StartCoroutine(StartTutorial());
